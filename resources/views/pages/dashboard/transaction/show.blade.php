3759f608f45d47d66b8909a01f645a0cacc737cf
aminbeno <aminebenhadare@gmail.com>
2026-04-14 00:29:59 +0100

    php: ^8.2

--- resources/views/pages/dashboard/transaction/show.blade.php
+++ resources/views/pages/dashboard/transaction/show.blade.php
@@ -41,7 +41,7 @@
                         </span>
                     </div>
                 </div>
-
+                
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
                     <div class="space-y-6">
                         <div>
